a7c9d815232885a6f3d9d44ebb80d86716790942
GitFlzy <lzy2900@outlook.com>
2019-03-03 00:31:30 +0800

    修改了主页指向

---
+++ routes/index.py
@@ -0,0 +1,20 @@
+from flask import (
+    render_template,
+    request,
+    redirect,
+    session,
+    url_for,
+    Blueprint,
+    make_response,
+)
+
+# from models.user import User
+
+from utils import log
+
+main = Blueprint('index', __name__)
+
+@main.route("/")
+def index():
+    # u = current_user()
+    return redirect(url_for('blog.index'))

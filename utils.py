ed74bb92dc455cd0f3fada9ecbd592431fbfd4e4
GitFlzy <lzy2900@outlook.com>
2020-05-21 00:26:42 +0800

    更新 log 方式
    按日期生成 log 文件

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import time
 
-
 def log(*args, **kwargs):
     format = '%H:%M:%S'
     value = time.localtime(int(time.time()))
     dt = time.strftime(format, value)
-    with open('log.gua.txt', 'a', encoding='utf-8') as f:
+    time_format = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
+    filename = time_format + '.txt'
+    with open(filename, 'a', encoding='utf-8') as f:
         print(dt, *args, file=f, **kwargs)
